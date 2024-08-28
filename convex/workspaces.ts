c5d0345da7b0052f22d524911d327b438d51f003
oliskevics <olishkevich123@gmail.com>
2024-08-28 21:47:17 +0200

    added update and remove flow

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,80 +1,135 @@
-import { v } from 'convex/values';
-import { mutation, query } from './_generated/server';
-import { getAuthUserId } from '@convex-dev/auth/server';
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
 
 const generateCode = () => {
-	const code = Array.from(
-		{ length: 6 },
-		() => '0123456789qwertyuiopasdfghklzxcvbnm'[Math.floor(Math.random() * 36)]
-	).join('');
+  const code = Array.from(
+    { length: 6 },
+    () => "0123456789qwertyuiopasdfghklzxcvbnm"[Math.floor(Math.random() * 36)]
+  ).join("");
 
-	return code;
+  return code;
 };
 
 export const create = mutation({
-	args: { name: v.string() },
-	handler: async (ctx, args) => {
-		const userId = await getAuthUserId(ctx);
-		if (!userId) throw Error('Unauthorized');
+  args: { name: v.string() },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw Error("Unauthorized");
 
-		const joinCode = generateCode();
+    const joinCode = generateCode();
 
-		const workspaceId = await ctx.db.insert('workspaces', {
-			name: args.name,
-			userId,
-			joinCode,
-		});
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name: args.name,
+      userId,
+      joinCode,
+    });
 
-		await ctx.db.insert('members', {
-			userId,
-			workspaceId,
-			role: 'admin',
-		});
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
 
-		return workspaceId;
-	},
+    return workspaceId;
+  },
 });
 
 export const get = query({
-	args: {},
-	handler: async ctx => {
-		const userId = await getAuthUserId(ctx);
-		if (!userId) return [];
+  args: {},
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) return [];
 
-		const members = await ctx.db
-			.query('members')
-			.withIndex('by_user_id', q => q.eq('userId', userId))
-			.collect();
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
 
-		const workspacesIds = members.map(el => el.workspaceId);
-		const workspaces = [];
+    const workspacesIds = members.map((el) => el.workspaceId);
+    const workspaces = [];
 
-		for (const workspaceId of workspacesIds) {
-			const workspace = await ctx.db.get(workspaceId);
+    for (const workspaceId of workspacesIds) {
+      const workspace = await ctx.db.get(workspaceId);
 
-			if (workspace) {
-				workspaces.push(workspace);
-			}
-		}
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
 
-		return workspaces;
-	},
+    return workspaces;
+  },
 });
 
 export const getById = query({
-	args: { id: v.id('workspaces') },
-	handler: async (ctx, args) => {
-		const userId = await getAuthUserId(ctx);
-		if (!userId) throw Error('Unauthorized');
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw Error("Unauthorized");
 
-		const member = await ctx.db
-			.query('members')
-			.withIndex('by_workspace_id_user_id', q =>
-				q.eq('workspaceId', args.id).eq('userId', userId)
-			)
-			.unique();
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
 
-		if (!member) return null;
-		return await ctx.db.get(args.id);
-	},
+    if (!member) return null;
+    return await ctx.db.get(args.id);
+  },
+});
+
+export const update = mutation({
+  args: { id: v.id("workspaces"), name: v.string() },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw Error("Unauthorized");
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw Error("Unauthorized");
+    }
+
+    await ctx.db.patch(args.id, { name: args.name });
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw Error("Unauthorized");
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw Error("Unauthorized");
+    }
+
+    const [members] = await Promise.all([
+      ctx.db
+        .query("members")
+        .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
+        .collect(),
+    ]);
+
+    for (const member of members) {
+      await ctx.db.delete(member._id);
+    }
+
+    await ctx.db.delete(args.id);
+    return args.id;
+  },
 });
---
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -0,0 +1,128 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useRemoveWorkspace } from "@/features/workspaces/api/use-remove-workspace";
+import { useUpdateWorkspace } from "@/features/workspaces/api/use-update-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { TrashIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+
+interface Props {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  initialValue: string;
+}
+
+export const PreferencesModal = ({ initialValue, open, setOpen }: Props) => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const [editOpen, setEditOpen] = useState(false);
+  const [value, setValue] = useState(initialValue);
+
+  const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } =
+    useUpdateWorkspace();
+  const { mutate: removeWorkspace, isPending: isRemovingWorkspace } =
+    useRemoveWorkspace();
+
+  const handleEdit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    updateWorkspace(
+      { id: workspaceId, name: value },
+      {
+        onSuccess: () => {
+          toast.success("Ви успішно змінили назву");
+          setEditOpen(false);
+        },
+        onError: () => toast.error("Помилка при зміні назви"),
+      }
+    );
+  };
+
+  const handleRemove = () => {
+    removeWorkspace(
+      { id: workspaceId },
+      {
+        onSuccess: () => {
+          toast.success("Ви успішно видалили середовище");
+          router.replace("/");
+        },
+        onError: () => toast.error("Помилка при видаленні"),
+      }
+    );
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+        <DialogHeader className="p-4 border-b bg-white">
+          <DialogTitle>{value}</DialogTitle>
+        </DialogHeader>
+        <div className="px-4 pb-4 flex flex-col gap-y-2">
+          <Dialog open={editOpen} onOpenChange={setEditOpen}>
+            <DialogTrigger asChild>
+              <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-semibold">Імя середовища</p>
+                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                    Змінити
+                  </p>
+                </div>
+                <p className="text-sm">{value}</p>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Змінити назву для середовища</DialogTitle>
+              </DialogHeader>
+              <form className="space-y-4" onSubmit={handleEdit}>
+                <Input
+                  value={value}
+                  disabled={isUpdatingWorkspace}
+                  onChange={(e) => setValue(e.target.value)}
+                  required
+                  autoFocus
+                  minLength={3}
+                  maxLength={24}
+                  placeholder="Робота, Дім, тощо..."
+                />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button
+                      type="button"
+                      variant="outline"
+                      disabled={isUpdatingWorkspace}
+                    >
+                      Відмінити
+                    </Button>
+                  </DialogClose>
+
+                  <Button type="button" disabled={isUpdatingWorkspace}>
+                    Зберегти
+                  </Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
+          <button
+            disabled={isUpdatingWorkspace || isRemovingWorkspace}
+            onClick={handleRemove}
+            className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
+          >
+            <TrashIcon className="size-4" />
+            <p className="text-xs font-semibold">Видалити середовище</p>
+          </button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
